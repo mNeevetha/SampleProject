c81c01a758b56b55c4aa84a3b44975b70f5f7d6e
Neevetha <neevetha.m@cognizant.com>
2017-03-31 18:13:37 +0530

    altered

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get('/', function(req, res){
     res.send("Hello World 31stMar !!!");
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!')
 })
