efbcd9eae559bcaa5e0d6fb69ab1d13a392d06d7
mNeevetha <neevetha.m@cognizant.com>
2017-03-31 18:22:47 +0530

    Changes server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 8080));
 
 app.get('/', function(req, res){
     res.send("Hello World 31stMar !!!");
 })
 
-app.listen(8080, function () {
+app.listen(app.get('port'), function () {
   console.log('Example app listening on port 8080!')
 })
